233cf07783d1b557da71b48ca94b3e07681b82ed
miryalakavya <45136999+miryalakavya@users.noreply.github.com>
2023-05-05 20:06:02 -0500

    Wrong Path

--- Code/controller/AdminInterface.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import javafx.event.ActionEvent;
-
-public abstract class AdminInterface {
-	
-	public abstract void Authorize(ActionEvent event);
-	public abstract void Home(ActionEvent event);
-}
