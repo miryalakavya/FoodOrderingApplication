19d3de9af8bcde128d439c97f072911845152149
dileepkumarkethe <63191518+dileepkumarkethe@users.noreply.github.com>
2023-05-05 20:10:04 -0500

    created Admin Fuctionalities

---
+++ Code/src/controller/AdminInterface.java
@@ -0,0 +1,9 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public abstract class AdminInterface {
+	
+	public abstract void Authorize(ActionEvent event);
+	public abstract void Home(ActionEvent event);
+}
